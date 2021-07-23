59afcb112e8bac4965b62fd371bb6dbd1130e17f
Levi-Opunga <leviopunga@gmail.com>
2021-07-23 19:18:55 +0300

    completed upto footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-            $(#design).hover(function() {
-                    $(#desig).append( < h3 > design < /h3> <
-                        p > Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing.Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed. <
-                        /p> );
-                    })
-            })
+$(document).ready(function () {
+  $("img#design").click(function () {
+    $("img#design").append(
+      " <p > Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing.Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed. </p> "
+    );
+  });
+});
